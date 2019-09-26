aff7638a3f877488685dd2e9741dfb56f7715ff8
Mauro Petrini <dev.mpetrini@gmail.com>
2019-09-25 23:12:55 -0300

    add clase 01 profe

---
+++ Clase 01/Profe/Ejercicio/js/index.js
@@ -0,0 +1,13 @@
+var nombre = prompt('Ingrese su nombre: ');
+console.log('Hola ' + nombre);
+
+// var numeroEnMemoria = 10;
+// var numeroIngresado = prompt('Ingrese un numero: ');
+// console.log(numeroEnMemoria + parseInt(numeroIngresado));
+
+// var numeroIngresadoParaResta = prompt('Ingrese un nuevo numero: ');
+// console.log(numeroEnMemoria - parseInt(numeroIngresadoParaResta));
+
+// var numero1 = parseInt(prompt('Ingrese el primero numero: '));
+// var numero2 = parseInt(prompt('Ingrese el segundo numero: '));
+// console.log(numero1 + numero2);
---
+++ Clase 01/Profe/En clase/ejemplos.js
@@ -0,0 +1,147 @@
+//Mi primer comentario
+console.log('Primer comentario');
+
+/*
+Esto es 
+un comentario
+de varias lineas
+*/
+console.log('Segundo comentario');
+
+//--------------------- °°°°° ---------------------
+
+////Sintaxis y palabras reservadas
+
+// var firstName;
+// firstName = 'Juan';
+
+// if (firstName === 'Juan') {
+//   alert('Tu nombre es Juan!');
+// }
+
+// console.log('Hola ' + firstName);
+
+//--------------------- °°°°° ---------------------
+
+////Variables
+
+// var firstName;
+// firstName = 'Juan';
+// firstName = 'Pedro';
+
+// var $precio;
+// $precio = 25;
+
+// var _texto;
+// _texto = 'Soy un texto';
+
+// var texto1;
+// texto1 = 'Primer texto';
+
+//--------------------- °°°°° ---------------------
+
+////Tipos de valores
+
+// var boolean1 = true;
+// var boolean2 = false;
+
+// var intNumber = 3;
+// var floatNumber = 0.8732;
+// var negativeFloatNumber = -5.4;
+
+// var firstName = 'Juan';
+// var email = 'juan@gmail.com';
+// var numbers = '1 2 3 4 5 setenta y ocho';
+// var text = 'The dark side of the moon';
+
+// var example1; //undefined
+// var example2 = null; //null
+
+// var array = [1, 2, 'Pedro', false];
+// console.log(array[2]);
+// console.log(array[3]);
+
+// var user = {
+//   firstName: 'Juan',
+//   age: 25,
+//   address: 'Calle falsa 1234'
+// };
+
+// console.log('Datos del usuario: ');
+// console.log(user.firstName, user.age, user.address);
+
+//--------------------- °°°°° ---------------------
+
+////Operadores aritmeticos
+
+////Asignacion
+var total;
+total = 2.5;
+
+// var message = 'Hola Mundo';
+
+////Suma
+total = 2 + 2;
+
+// var number1 = 2.5;
+// var number2 = 4;
+// console.log(number1 + number2);
+
+// var total;
+console.log(total * 5);
+
+////Concatenacion
+// console.log('Hola ' + 'mundo');
+// console.log('Hola ' + 234);
+// console.log('Hola ' + true);
+// var firstName = 'Juan';
+// var message = 'Hola ' + firstName;
+
+// console.log(message);
+
+////Resta
+// var subtotal = 8;
+// console.log(subtotal - 10);
+// console.log(subtotal - '2');
+// console.log(subtotal - 'HOLA');
+
+////Multiplicacion
+// var subtotal = 5;
+// var total = subtotal * 10;
+// console.log(total);
+// console.log(subtotal * '3');
+// console.log(subtotal * 'HOLA');
+
+////Division
+// var subtotal = 50;
+// var total = subtotal / 10;
+// console.log(total);
+// console.log(2 / 0);
+// console.log(2 / 'HOLA');
+
+////Modulo
+// console.log(5 % 2);
+// console.log(4 % 2);
+// console.log(50 % 7);
+// console.log(10 % '5');
+// console.log(10 % 'HOLA');
+
+////Incremental
+// var subtotal = 5;
+// console.log(subtotal++);
+
+// var total = '2';
+// console.log(total++);
+
+// var text = 'Hola';
+// console.log(text++);
+
+////Decremental
+// var subtotal = 5;
+// console.log(subtotal--);
+
+// var total = '2';
+// console.log(total--);
+
+// var text = 'Hola';
+// console.log(text--);
