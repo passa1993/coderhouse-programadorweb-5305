fc3139b228025d317a96e81b563157730e170099
Mauro Petrini <dev.mpetrini@gmail.com>
2019-09-30 23:22:29 -0300

    agregue clase 03

---
+++ Clase 03/Profe/En clase/js/index.js
@@ -0,0 +1,46 @@
+////Ciclos
+//// FOR
+// for (var i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// var daysOfTheWeek = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+// for (let i = 0; i < daysOfTheWeek.length; i++) {
+//   const day = daysOfTheWeek[i];
+//   console.log(day);
+// }
+
+var i = 1;
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 10);
+
+// var value;
+// do {
+//   value = prompt('Ingrese un valor mayor a 5');
+// } while (value <= 5);
+
+// console.log(value);
+
+//--------------------- °°°°° ---------------------
+
+////Break y continue
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+//   if (i === 5) {
+//     break;
+//   }
+// }
+
+// for (let i = 0; i < 10; i++) {
+//   if (i === 5) {
+//     continue;
+//   }
+//   console.log(i);
+// }
