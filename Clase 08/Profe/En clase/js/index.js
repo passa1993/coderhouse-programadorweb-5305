16be060b0da44286232af38ae2fb7b5d17422b57
Mauro Petrini <dev.mpetrini@gmail.com>
2019-10-16 23:24:51 -0300

    agregue clase 08

---
+++ Clase 08/Profe/En clase/js/index.js
@@ -0,0 +1,43 @@
+////Nodos, acceso, crear y eliminar
+// let titleNode = document.getElementById('main-title');
+// titleNode.innerHTML = 'Al fin voy a ver algo en mi página';
+
+// let paragraphNodes = document.getElementsByClassName('paragraph');
+// for (let i = 0; i < paragraphNodes.length; i++) {
+//   const node = paragraphNodes[i];
+//   node.innerHTML = 'Soy el párrafo numero ' + i;
+// }
+
+// let body = document.querySelector('body');
+
+// let paragraphNode = document.createElement('div');
+// paragraphNode.innerHTML = '<p>Soy un párrafo</p>';
+
+// body.appendChild(paragraphNode);
+
+// let list = ['Elemento 1', 'Elemento 2', 'Elemento 3'];
+// let listNode = document.createElement('ul');
+
+// for (let i = 0; i < list.length; i++) {
+//   const element = list[i];
+//   let li = document.createElement('li');
+//   li.innerHTML = element;
+
+//   listNode.appendChild(li);
+// }
+
+// body.appendChild(listNode);
+
+// let paragraphWithId = document.getElementById('paragraph');
+// // paragraphWithId.parentNode.removeChild(paragraphWithId);
+
+// paragraphWithId.parentNode.innerHTML = '<h1>Soy un titulo ahora</h1>';
+
+////Propiedades
+// let id = document.getElementById('title').id;
+// console.log(id);
+// document.getElementById('title').id = 'pato';
+// document.getElementById('list-item').className = "list-group-item";
+
+// let value = document.getElementById('text-input').value;
+// console.log(value);
